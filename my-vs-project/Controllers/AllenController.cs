93e8a1e690061c4b81fc1da61a28b75155da655b
Allen <rtallen226@gmail.com>
2022-06-20 18:40:41 +0800

    test

--- my-vs-project/Controllers/AllenController.cs
+++ my-vs-project/Controllers/AllenController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using my_vs_project.Models;
 
 namespace my_vs_project.Controllers
 {
     public class AllenController : Controller
     {
+        NorthwindContext _context;
+        public AllenController(NorthwindContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             ViewBag.text = "Hello World";
             return View();
         }
-        
+        public IActionResult News()
+        {
+            return RedirectToAction("Index");
+        }
     }
 }
