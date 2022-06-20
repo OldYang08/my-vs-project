55e5ca5d75782ca66a0e1e0c66678935f6955d2a
OldYang <homkins8@gmail.com>
2022-06-20 16:48:34 +0800

    no message

---
+++ my-vs-project/Controllers/AllenController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_vs_project.Controllers
+{
+    public class AllenController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.text = "Hello World";
+            return View();
+        }
+        
+    }
+}
