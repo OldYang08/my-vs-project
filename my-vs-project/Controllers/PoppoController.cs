2b67e9b5092fd1ea4f1d5e7ffcd6e499a741576e
阿暉伯 <poppoman@gmail.com>
2022-06-20 15:25:38 +0800

    add poppo

---
+++ my-vs-project/Controllers/PoppoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_vs_project.Controllers
+{
+    public class PoppoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
