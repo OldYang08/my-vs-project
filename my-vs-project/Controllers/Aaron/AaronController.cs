d305670e6f2b468ce41a5ea702db92f733842345
AaronChen <zu00731135@gmail.com>
2022-06-20 15:17:49 +0800

    Add Aaron

---
+++ my-vs-project/Controllers/Aaron/AaronController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_vs_project.Controllers.Aaron
+{
+    public class AaronController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
